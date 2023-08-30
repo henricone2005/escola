bad5e9ccfe298d17823f908a405fe1c9474103e2
henricone2005 <rm550981@fiap.com.br>
2023-08-30 12:12:15 -0300

    lição gemini

---
+++ src/main/java/com/example/Credencial.java
@@ -0,0 +1,17 @@
+package com.example;
+
+
+public record Credencial (String Senha, String Site) {
+
+    public static void verificar(Credencial chave) {
+    }
+
+    public static void add(Credencial txtSenha) {
+    }
+
+    public String getText() {
+        return null;
+    }
+
+}
+
---
+++ src/main/java/com/example/PrimaryController.java
@@ -0,0 +1,119 @@
+package com.example;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+public class PrimaryController {
+    // textField e TextArea são objetos/classes
+    @FXML
+    ListView<Credencial>listSenha;
+    @FXML
+    TextField txtChave;
+    @FXML
+    TextField txtSite;
+    @FXML
+    TextField txtSenha;
+    @FXML
+    Button salvar;
+    
+
+    // collections em java - ArrayList
+    // <String> - generics
+    // inner class x class
+    // (classe com metodos de uso interno usaria ele aqui dentro)
+    //private ArrayList<Credencial> cre = new ArrayList<>();
+
+    public void adicionarCredencial() {
+        var credencial = new Credencial(txtSite.getText(), (txtSenha.getText()) );
+
+        //conectar com o banco
+        
+        final String USER = "rm99004";
+        final String PASS = "24466666";
+        
+        final String URL = "jdbc:oracle:thin:@oracle.com.br:1521:orcl";
+
+        try {
+            var con = DriverManager.getConnection(URL, USER, PASS);
+            //statement
+            // var instrucao = con.createStatement();
+            // var sql = "INSERT INTO alunos (nome, turma, rm) VALUES ('"+ aluno.nome() + "', '"+ aluno.turma() +"', "+ aluno.rm() +" )";
+            // instrucao.execute(sql);
+
+            var sql = "INSERT INTO Credencial ( Site, Senha) VALUES ( ?, ? )";
+            var instrucao = con.prepareStatement(sql);
+            instrucao.setString(1, credencial.Site());
+            instrucao.setString(2, credencial.Senha());
+            instrucao.executeUpdate();
+
+            Alert alert = new Alert(AlertType.INFORMATION, "Salvo com sucesso");
+            alert.show();
+
+            con.close();
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+
+        //executar o insert
+
+        //desconectar
+
+
+       
+        // txtAlunos.appendText(txtNome.getText() + "\n");
+        txtSite.clear();
+
+        adicionarCredencial();
+    }
+
+    public void mostraeCredencial() {
+        //conectar com o banco
+        final String USER = "rm99004";
+        final String PASS = "24466666";
+        
+        final String URL = "jdbc:oracle:thin:@oracle.com.br:1521:orcl";
+
+        try {
+            var con = DriverManager.getConnection(URL, USER, PASS);
+
+            var sql = "SELECT * FROM credenciais ORDER BY site";
+            var instrucao = con.prepareStatement(sql);
+            var dados = instrucao.executeQuery();
+
+            while(dados.next()){
+                var credencial = new Credencial(
+                    dados.getString("Chave"),
+                    dados.getString("Site")
+                );
+                listSenha.getItems().add(credencial);
+            }
+
+            con.close();
+
+
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+
+    public void verificarCredencial() {
+       if(txtChave.getText().toString().equals("1234567")){
+        System.out.println("Ta certo miseravi");
+        salvar.setDisable(true);
+       }
+       else{
+        System.out.println("errado");
+        salvar.setDisable(false);
+       }
+    
+    }
+}
